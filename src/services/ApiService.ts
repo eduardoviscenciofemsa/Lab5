9df29c33a916816d1cb3e987a8212c8f75d7b200
eduardoviscenciofemsa <jesus.oliva@digitalfemsa.com>
2024-11-28 20:12:21 -0600

    feat: first commit

---
+++ src/services/ApiService.ts
@@ -0,0 +1,22 @@
+import axios, {type AxiosError} from 'axios';
+
+import type {ResponseUserT} from '../types/response.type';
+
+import {storeData} from './StorageService';
+
+export const getUsers = async ({queryName = ''}: {queryName?: string} = {}) => {
+  console.log('queryName:', queryName);
+  try {
+    const {data} = await axios.get<ResponseUserT[]>(
+      `https://jsonplaceholder.typicode.com/users${
+        queryName ? `?q=${queryName}` : ''
+      }`,
+    );
+
+    await storeData('users', data);
+
+    return data;
+  } catch (err) {
+    throw new Error((err as AxiosError).message);
+  }
+};
---
+++ src/types/response.type.ts
@@ -0,0 +1,4 @@
+export type ResponseUserT = {
+  id: number;
+  name: string;
+};
